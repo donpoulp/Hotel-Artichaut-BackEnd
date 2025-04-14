8addd913fca845c57a3f17931e7a8a8cf0e17db3
Bastien Pelletier <bastien.pelletier@le-campus-numerique.fr>
2025-04-14 15:14:56 +0200

    test

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_register(): void
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_login(): void
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+}
