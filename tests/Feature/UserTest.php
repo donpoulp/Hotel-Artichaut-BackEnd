d6b31954c13bcf3f6e158f124ffdb327e2f18af5
Bastien Pelletier <bastien.pelletier@le-campus-numerique.fr>
2025-04-14 15:27:27 +0200

    user test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -11,10 +11,33 @@ class UserTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_allUsers(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/user');
+        $response->assertStatus(200);
+    }
 
+    public function test_UserShowid(): void
+    {
+        $response = $this->get('/api/user/2');
+        $response->assertStatus(200);
+    }
+
+    public function test_UpdateUser(): void
+    {
+        $response = $this->put('/api/user/2');
+        $response->assertStatus(200);
+    }
+
+    public function test_PostUser(): void
+    {
+        $response = $this->post('/api/user');
+        $response->assertStatus(200);
+    }
+
+    public function test_DeleteUser(): void
+    {
+        $response = $this->delete('/api/user/5');
         $response->assertStatus(200);
     }
 }
