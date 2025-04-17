2d3a232de4c6776c020e11779c2908017bd11d7b
Bastavavos <bastien.pelletier@orange.fr>
2025-04-17 11:07:32 +0200

    add bast admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,6 +42,18 @@ class DatabaseSeeder extends Seeder
             'created_at' => fake()->dateTime,
             'updated_at' => fake()->dateTime,
         ]);
+        DB::table('users')->insert([
+            'firstname' => Str('Bast'),
+            'lastname' => Str('b'),
+            'email' => Str('bastien.pelletier74@gmail.com'),
+            'emailBis' => Str('rien@gmail.com'),
+            'password' => Hash::make('bast123456'),
+            'phone' => fake()->phoneNumber,
+            'phoneBis' => fake()->phoneNumber,
+            'is_admin' => 1,
+            'created_at' => fake()->dateTime,
+            'updated_at' => fake()->dateTime,
+        ]);
         //BEDROOM TYPE**************************************************************************************************
         DB::table('bedroom_type')->insert([
             'nameFr' => Str('Suite Royale'),
