797579b13a3fdc847118f9dec8899e07a2e359d9
Raphael Petrozzi <raphael.petrozzi@le-campus-numerique.fr>
2025-04-23 12:05:27 +0200

    Raph correction dernier push manque de api.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,6 @@ use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
